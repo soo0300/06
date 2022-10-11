156cae8d2f718e90333d8f9c5ad5877360580ca8
soo0300 <soo0300@naver.com>
2022-10-11 17:25:05 +0900

    실습5

--- main.c
+++ main.c
@@ -1,23 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+int get_integer(){
+	int num; 
+	printf("정수를 입력하세요: " );
+	scanf("%d",&num);
+	return num;
+}
 
-int square(int a){
-	a*=a;
+int combination(){
+	int n=get_integer();
+	int r=get_integer();
+	
+	int up,down,down2;
+	up = func1(n); /* n 팩토리얼*/
+	down =  func1(n-r); /*(n-r) 팩토리얼*/
+	down2 = func1(r); /* r 팩토리얼*/
+	
+	return up/(down*down2);
+	
+	
+}
+
+int func1(int num){
+	int i=num;
+	int sum=1;
+	for(i; i>0; i--){
+		sum*=i;
+	}
+	return sum;
 }
 
 int main(void){
-	/*
-	call by value에 의해 
-	a=2
-	square(a);옴 
-	printf("a=%i",a);
-	의 값은 2가 나옴, 밑의 코드가 옳은 코드 
-	*/
+	int result = combination();
 	
-	int a=2;
-	a=square(a);
-	printf("a=%i\n",a);
+	printf("%d",result);
 
+	
 	return 0;
 		
 }
